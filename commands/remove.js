6a77de48e06dbb11dad39801b42194d0af953df3
puffindragonsguild <puffindragonsguild@gmail.com>
2026-05-13 11:13:03 +0100

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,20 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-    name: 'remove',
-    description: 'Removes a specific character from the signups.',
+    data: new SlashCommandBuilder()
+        .setName('remove')
+        .setDescription('Removes a specific character from the raid signups.')
+        .addStringOption(option => option.setName('character')
+            .setDescription('Exact character name')
+            .setRequired(true)),
     adminOnly: true,
-    execute(message, args, client, db, raidManager) {
-        const charName = args.join(' ').trim();
-        
-        if (!charName) {
-            return message.reply('❌ Usage: `!remove [Character Name]`');
-        }
+    async execute(interaction, client, db, raidManager) {
+        const charName = interaction.options.getString('character').trim();
 
         const info = db.prepare('DELETE FROM signups WHERE LOWER(character_name) = LOWER(?)').run(charName);
         if (info.changes > 0) {
-            message.reply(`🗑️ Purged: ${charName} has been removed.`);
-            raidManager.displayRoster(message.channel);
+            await interaction.reply(`🗑️ Purged: **${charName}** has been removed.`);
+            raidManager.displayRoster(interaction.channel);
         } else {
-            message.reply(`The Queen does not acknowledge your existence.❓ Character ${charName} not found.`);
+            await interaction.reply(`The Queen does not acknowledge your existence. ❓ Character **${charName}** not found.`);
         }
     },
 };
